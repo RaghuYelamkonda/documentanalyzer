069907380d40cc7fcfc0a7ae0fe1081a1c838eb2
Rajani <rajani.yelamkonda@gmail.com>
2018-08-05 11:54:27 -0400

    Basic functionality

---
+++ src/main/resources/web/src/app/shared/service/documents.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+import { Document } from 'src/app/shared/model/Document';
+import { AuthenticationService } from 'src/app/shared/service/authentication.service';
+import { User } from 'src/app/shared/model/User';
+
+@Injectable()
+export class DocumentsService {
+    documents: Document[];
+
+    constructor(private http: HttpClient, private authenticationService: AuthenticationService) { }
+
+    getDocuments() {
+        var user: User = this.authenticationService.getLoggedInUser();
+        if(user) {
+            console.log("Hitting url "+ `/api/v1/document/`+user.id)
+            return this.http.get<any>(`/api/v1/document/`+user.id)
+            .pipe(map(documents => {
+                this.documents = <Document[]>documents;
+                return documents;
+            }));
+        }
+    }
+
+    reloadDocuments() {
+        var user: User = this.authenticationService.getLoggedInUser();
+        if(user) {
+            this.documents.length=0;
+            console.log("Hitting url "+ `/api/v1/document/`+user.id)
+            return this.http.get<any>(`/api/v1/document/`+user.id)
+            .pipe(map(docs => {
+                console.log(docs);
+                docs.forEach(element => {
+                    this.documents.push(element);
+                });
+            }));
+        }
+    }
+
+    refreshDocuments() {
+        this.documents.length = 0;
+        this.getDocuments();
+    }
+}
